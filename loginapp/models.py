6c7acd2ad89b1317749fa7880756893107d769a0
shireesha-apalya <51369864+shireesha-apalya@users.noreply.github.com>
2019-09-17 14:00:50 +0530

    Update models.py

--- loginapp/models.py
+++ loginapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
-class Student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=80)
-class Employee(models.Model):
-    eno = models.CharField(max_length=10)
-    esal = models.CharField(max_length=10)
 
+class Person(models.Model):
+    User_name = models.CharField(max_length=10)
+    Password = models.CharField(max_length=10)
+
+
+    class Meta:
+        db_table = "Person"
