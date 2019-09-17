938c8210f4fcacb30d8b7567a0135b1ae488cdcb
shireesha-apalya <51369864+shireesha-apalya@users.noreply.github.com>
2019-09-17 13:59:41 +0530

    Update views.py

--- loginapp/views.py
+++ loginapp/views.py
@@ -3,27 +3,32 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth import get_user_model, authenticate, login, logout
-
+from login_app.models import Person
 
 # Create your views here.
-
-
 def user_registration(request):
     request_method = request.method
+
     if request_method == "POST":
+
         user_name = request.POST["UserName"]
         password = request.POST["Password"]
+
         if len(user_name) > 0 and len(password) > 0:
             user = authenticate(request, username=user_name, password=password)
+            print("users+",user)
             if user is None:
                 user = get_user_model().objects.create_user(username=user_name, password=password)
+                signup_user = Person.objects.create(User_name=user_name, Password=password)
+                user.save()
+
                 if user is not None:
                     user.is_staff = True
                     user.save()
-                context = {'message': f'Hi {user_name}, you have Successfully registered your account !!!'}
+                context = {'message': 'Hi {}, you have Successfully registered your account !!!'.format(user_name)}
                 return render(request, 'user_login.html', context)
             else:
-                context = {'message': f'Hi {user_name}, you have already hold an account !!!'}
+                context = {'message': 'Hi {}, you have already hold an account !!!'.format(user_name)}
                 return render(request, 'user_login.html', context)
     return render(request, 'user_registration.html')
 
@@ -34,12 +39,14 @@ def user_login(request):
         username = request.POST['UserName']
         password = request.POST['Password']
         user = authenticate(request, username=username, password=password)
+        print(user)
         if user is not None:
             login(request, user)
-            context = {"message": f"Welcome {username} !!!"}
+            context = {"message": "Welcome {} !!!".format(username)}
+            return resp
             return render(request, 'user_page.html', context)
         else:
-            context = {"message": f"{username}, Try Again !!!"}
+            context = {"message": "{}, Try Again !!!".format(username)}
             return render(request, 'user_login.html', context)
     return render(request, 'user_login.html')
 
